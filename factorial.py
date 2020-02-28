242e50e291602c7300650f93e84f8d1bac5f79ac
Anshdeep Singh <anshdeep1494@gmail.com>
2020-02-27 21:40:40 -0400

    updated factorial program

--- factorial.py
+++ factorial.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
+import csv
+import time
 
-def fact(n):
+
+fact_results = open("fact_results.csv", 'w+', encoding='utf-8')
+headings = "Request Id,time,N,Result\n"
+fact_results.write(headings)
+
+
+
+def fact(n,rId):
+	start = time.time()
 	fact = 1
 	for i in range(1,n+1): 
 	    fact = fact * i 
-	print ("The factorial of " + str(n) + " is : " + str(fact)) 
+	print (str(fact)) 
+	stop = time.time()
+	duration = stop - start
+	row = str(rId) + ',' + str(duration)[:-15] + ',' + str(n) + ',' + str(fact) +  '\n'
+	fact_results.write(row)
+
 
 
 # Reading the value of n from input file
 f = open("input.txt", "r")
+rId = 1
 for n in f:
-  fact(int(n.strip()))
+
+  fact(int(n.strip()),rId)
+  rId = rId + 1
+
+
