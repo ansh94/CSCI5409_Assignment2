bec6223e1b519433e82f31ad5a34b14eb4f08826
Anshdeep Singh <anshdeep1494@gmail.com>
2020-02-27 23:12:26 -0400

    updated fibonacci program

--- fibonacci.py
+++ fibonacci.py
@@ -1,25 +1,43 @@
-  
-def fibonacci(n):  
-   if n <= 1:  
-       return n  
-   else:  
-       return(fibonacci(n-1) + fibonacci(n-2))  
+#!/usr/bin/env python3
+import csv
+import time
 
+fib_results = open("fib_results.csv", 'w+', encoding='utf-8')
+headings = "Request Id,time,N,Result\n"
+fib_results.write(headings)
   
 
   
 # Reading the value of n from input file
 f = open("input.txt", "r")
+rId = 1
 for n in f:
+	start = time.time()
+
+	strN = str(n)
+	fibResult = []
+
 	inputN = int(n)
 	a = 0
 	b = 1
 	print(a,b,end=" ")
+	fibResult.append(a)
+	fibResult.append(b)
 	while(inputN - 2):
 	    c = a + b
 	    a,b = b,c
 	    print(c, end=" ")
+	    fibResult.append(c)
 	    inputN = inputN-1
+
+	strArray = ''.join(str(e) for e in fibResult)
+
+	stop = time.time()
+	duration = stop - start
+	row = str(rId) + ',' + str(duration)[:-13] + ',' + str(int(n)) + ',' + strArray +  '\n'
+	fib_results.write(row)
+	rId = rId + 1
+
 
 	print()
 
